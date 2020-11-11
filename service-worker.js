96dd361370ea780aac0633837e3e1535b27f64b9
Alina <alina90565@gmail.com>
2020-11-11 13:42:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "./precache-manifest.005c33bdc7c66deb31950270df63ce5d.js"
 );
 
 self.addEventListener('message', (event) => {
